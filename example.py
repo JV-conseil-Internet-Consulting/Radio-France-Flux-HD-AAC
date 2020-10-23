5227d3cc2921a5c12771e79b627633edae5912e0
JV conseil <contact@jv-conseil.net>
2020-10-23 21:46:56 +0200

    mp3

--- example.py
+++ example.py
@@ -13,35 +13,33 @@ pages = [
 ]
 
 # Regex source: https://regex101.com/r/QzFpaY/1
-rgxaac = r"(?P<link>http[^,\"]*?/(?P<title>[^\.]*?)\-hifi\.aac\?id=radiofrance)"
-rgxmp3 = r"(?P<link>http[^,\"]*?/(?P<title>[^\.]*?)\.mp3\?id=radiofrance)"
+regex = {
+    "aac": r"(?P<link>http[^,\"]*?/(?P<title>[^\.]*?)\-hifi\.aac\?id=radiofrance)",
+    "mp3": r"(?P<link>http[^,\"]*?/(?P<title>[^\.]*?)\.mp3\?id=radiofrance)"
+}
 
-links = {"aac": {}, "mp3": {}}
+links = {k: {} for k, v in regex.items()}
 
 for p in pages:
     try:
         response = requests.get(p)
-        result = re.findall(rgxaac, response.text, 0)
-        links["aac"].update({y: x for x, y in result})
-        result = re.findall(rgxmp3, response.text, 0)
-        links["mp3"].update({y: x for x, y in result})
+        for k, rgx in regex.items():
+            result = re.findall(rgx, response.text, 0)
+            links[k].update({y: x for x, y in result})
     except Exception as e:
         print(e)
 
 # JSON View
-print()
-print("# JSON View")
-print(json.dumps(links, indent=1, ensure_ascii=False))
+print("\r\n# JSON View\r\n")
+print(json.dumps(links, indent=2, ensure_ascii=False))
 
 # Markdown View
-print()
-print("# Markdown View")
+print("\r\n# Markdown View\r\n")
 for x, y in links["aac"].items():
     print("- [%s](%s)" % (x, y))
 
 # M3U View
-print()
-print("#EXTM3U")
+print("\r\n#EXTM3U")
 for x, y in links["aac"].items():
     print("#EXTINF:0,Radio France HiFi - %s\r\n%s" % (x, y))
 print()
